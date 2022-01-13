71f5f0fe9b6f8fce12b47a545fbced621b842bef
Garvit Rajput <garvitrajput223@gmail.com>
2022-01-13 20:12:24 +0530

    Initial Commit

---
+++ assets/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Underdogidols</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="logo">
+         <a href="index.html"><img src="assets/img/udi.png"  alt="" class="img-fluid"></a>
+      </div>
+      <nav id="navbar" class="navbar" align="center" >
+        <ul>
+          <li><a class="nav-link scrollto active" href="#">HOME</a></li>
+          <li><a class="nav-link scrollto" href="#">CONTESTANTS</a></li>
+          <li><a class="nav-link scrollto" href="#">VIDEOS</a></li>
+          <li><a class="nav-link scrollto" href="#">ABOUT US</a></li>
+          <li><a class="nav-link scrollto" href="#">CONTACT US</a></li>
+          <li><a class="nav-link scrollto" href="#">BLOGS</a></li>
+          <li><a class="nav-link scrollto" href="#">HELP</a></li>
+         <a href="login.php">
+             <button class="btn btn-success">Login</button>
+         </a>
+         <a href="register.php">
+             <button class="btn btn-danger">Register</button>
+         </a>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+    </div>
+  </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Underdogidols</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+<?php
+    include 'assets/header.php';
+  ?>
+  <section id="hero">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+          <div data-aos="zoom-out">
+            <h1>Showcase your Talent with <span>Underdog Idols</span></h1>
+            <div class="text-center text-lg-start">
+              <a href="#about" class="btn-get-started scrollto">Get Started</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="300">
+          <img src="assets/img/hero-img2.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+
+
+
+  <main id="main">
+    <section id="pricing" class="pricing">
+      <div class="container">
+        <div class="section-title" data-aos="fade-up">
+          <h2>Pricing</h2>
+          <p>Choose Your Plan</p>
+        </div>
+        <div class="row" data-aos="fade-left">
+          <div class="col-lg-3 col-md-6">
+            <div class="box" data-aos="zoom-in" data-aos-delay="100">
+              <h3>BASIC</h3>
+              <h4><sup>$</sup>10<span> / month</span></h4>
+              <ul>
+                <li>✔ 1 Video upload per month/Season</li>
+                <li>✔ Video must be uploaded between day 1 and day 7 of the current month/season</li>
+                <li>✔ Promoting your video can start only after day 7 of the month/season</li>
+                <li>✔ Any videos uploaded after day 7 will go to next month/season competition</li>
+                <li>✔ Resources available for all contestants at Underdog Idols</li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="#" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mt-4 mt-md-0">
+            <div class="box featured" data-aos="zoom-in" data-aos-delay="200">
+              <h3>PREMIUM</h3>
+              <h4><sup>$</sup>99<span> / month</span></h4>
+              <ul>
+                <li>✔ Save 20 dollars/year</li>
+                <li>✔ 1 video upload per month/season</li>
+                <li>✔ Video can be uploaded at any day of the current month/season, without similar restrictions as basic plan</li>
+                <li>✔ Promotion of your video can start at anytime after you upload but still have until Day 30 of the month</li>   
+                <li>✔ Resources available for all contestants at Underdog Idols</li>          
+              </ul>
+              <div class="btn-wrap">
+                <a href="#" class="btn-buy">Buy Now</a>
+              </div>
+            </div>
+          </div>
+    <section>    
+
+  </main><!-- End #main -->
+
+
+
+
+
+
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>Contact Us</h3>
+              <p class="pb-3"><em>The leading platform for a second chance to stardom.</em></p>
+              <p>
+                Your Address Here<br><br>
+                <strong>Phone:</strong> +1-877-963-7283<br>
+                <strong>Email:</strong> info@underdogidols.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="youtube"><i class="bx bxl-youtube"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Current Season Contestant</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Past Season Winners</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Blog</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Our Support</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Sitemap</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Find Contestant</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>About Us</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quam aliquam ab expedita a architecto minus doloribus nesciunt maiores explicabo, voluptatum cupiditate necessitatibus minima libero, pariatur porro omnis labore. Amet?</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Underdogidols</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <a href="https://www.underdogidols.com">Underdogidols</a>
+      </div>
+    </div>
+  </footer>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
